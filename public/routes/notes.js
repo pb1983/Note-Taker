ba43b486eeb623d682a76e062ff3e8318101b035
pb1983 <phil.bryer@gmail.com>
2023-08-25 09:33:18 -0400

    added delete method

--- public/routes/notes.js
+++ public/routes/notes.js
@@ -6,7 +6,7 @@ fs = require("fs");
 
 //reads JSON file
 
-notes.get('/', (req, res) =>{
+notes.get('/', (req, res) => {
     res.json(db)
 }
 );
@@ -17,55 +17,73 @@ notes.get('/', (req, res) =>{
 notes.post('/', (req, res) => {
 
     const { title, text } = req.body
-  
+
     if (title && text) {
         const logNote = {
             title,
             text,
             id: randomString.generate(4)
         };
-  
+
         db.push(logNote)
-  
+
         const noteString = JSON.stringify(db, null, 2)
-  
+
         fs.writeFile("./public/db/db.json", noteString, (err) => {
             if (err) {
                 console.error(err)
-                return res.status(400).json('Error in posting review');
+                return res.status(400).json('Error in posting note');
             } else {
                 console.log(`Note for ${logNote.title} has been written to JSON file`);
-  
+
                 const response = {
                     status: 'success',
                     body: logNote,
                 };
-                
+
                 return res.status(201).json(response);
             }
         });
-            
-            
-        } 
-        
-        else {
-            res.status(500).json('Error in posting review');
-        }
-        
-        
-  });
+
+
+    }
+
+    else {
+        res.status(500).json('Error in posting note');
+    }
+
+
+});
 
 //Identifies the unique ID associated with each note on the page
 
 notes.get('/:id', (req, res) => {
- const requestedId = req.params.id;
- let [id] = db.filter((id) => id === requestedId)
- if (id)
- return res.json(id)
+    const requestedId = req.params.id;
+    let id = db.filter((id) => id === requestedId)
+    if (id)
+        return res.json(id)
 });
 
 
+//Removes note from JSON file when 'delete' icon is clicked
 
+ 
+notes.delete('/:id', (req, res) => {
+
+    const deleteId = req.params.id;
+    let deleteNote = db.filter(note => note.id !== deleteId)
+
+    const newString = JSON.stringify(deleteNote, null, 2)
+
+    fs.writeFile("./public/db/db.json", newString, (err) => {
+          if (err) {
+            console.error(err)
+            return res.status(400).json('Error in deleting item');
+        }
+    });
+
+    return res.json(newString)
+});
 
 
 module.exports = notes;
